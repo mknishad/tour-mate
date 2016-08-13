3a10d54904639705054a7ee0df8ca9976df3bfd6
Monir Hossain <mknishad1@ymail.com>
2016-08-13 23:08:11 +0600

    Initial commit - add activities

---
+++ app/src/main/java/com/example/nishad/tourmate/AddMomentExpenseActivity.java
@@ -0,0 +1,13 @@
+package com.example.nishad.tourmate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMomentExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_moment_cost);
+    }
+}
