413937b9af48a7a8d3b4896ae707ed47f146e8ee
Rubuya Tabassum <rubuyatabassum09@gmail.com>
2016-08-18 04:10:15 -0700

    add onek kichu

--- app/src/main/java/com/example/nishad/tourmate/activity/LoginActivity.java
+++ app/src/main/java/com/example/nishad/tourmate/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.nishad.tourmate.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.nishad.tourmate.R;
 
@@ -11,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToSignUp(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
